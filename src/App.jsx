f371a40c4a44ea671fa43faff290cfb3fdcc6d60
mi hector <hectorlandaezopsu@gmail.com>
2024-01-08 13:52:12 -0500

    jwt create

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
-
+/*  */
 function App() {
 
   const backgroundTema = localStorage.getItem("theme");
